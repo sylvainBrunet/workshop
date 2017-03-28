c249b3fa2fc5effdb842dc2a21caa0f634abdd44
sylvainBrunet <sylvain69740@gmail.com>
2017-03-28 16:27:10 +0200

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule}   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SimulatorComponent } from './simulator/simulator.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    SimulatorComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+        {
+          path: '',
+          redirectTo: '/home',
+          pathMatch: 'full'
+
+        },
+        {
+          path: 'home',
+          component: HomepageComponent
+        },
+        {
+          path: 'resultat',
+          component: ResultComponent
+        },
+        {
+          path: 'simulateur',
+          component: SimulatorComponent
+        },
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
